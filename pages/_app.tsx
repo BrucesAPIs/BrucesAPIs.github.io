38c0cfa042b722d9a2d8ab44532806fb3019a0d1
Bruce <joqtdepy@duck.com>
2025-01-19 15:29:08 +0800

    fix: tailwindcss didnot work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp 
